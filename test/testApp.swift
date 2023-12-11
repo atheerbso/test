40cc9b15ad15c525f1c77a8a9a96b8e96ffeb6e8
atheer <atheerbso@gmail.com>
2023-12-11 10:31:31 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Atheer on 27/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
